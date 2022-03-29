9e8a5710855ab95bc73669f9354c7bd7746c503c
mustaphaouassit <mustaphaoussit60gmail.com>
2022-03-29 19:17:02 +0000

    add module 01 ex02

---
+++ Module 01/ex 02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mustapha <mustapha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 17:59:39 by mustapha          #+#    #+#             */
+/*   Updated: 2022/03/29 19:13:32 by mustapha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string& stringREF = string;
+
+    std::cout << "The memory address of the string variable: " << &string << std::endl;
+    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+    std::cout << "The value of the string variable: " << string << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+    
+}
