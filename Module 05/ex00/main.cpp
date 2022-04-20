10561503f7a231d6ac9d87d52a5b579e1d595f93
Mustapha Ouassit <mouassit@e1r11p2.1337.ma>
2022-04-20 22:19:47 +0000

    add ex00 in module 05

---
+++ Module 05/ex00/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mouassit <mouassit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 10:56:58 by mouassit          #+#    #+#             */
+/*   Updated: 2022/04/20 22:17:54 by mouassit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+
+int main() 
+{
+	Bureaucrat test("test", 150);
+	try 
+	{
+        test.decrementBureaucrat();
+    }
+	catch (std::exception &e)
+	{
+        std::cout << e.what() << std::endl;
+    }
+	try
+	{
+		test.decrementBureaucrat();
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
+    return (0);
+}
