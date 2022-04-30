917d09c2bc2599af4f995b3072c68e814c569fb5
Mustapha Ouassit <mouassit@e1r13p12.1337.ma>
2022-04-30 14:55:00 +0000

    fix main ex02

--- Module 07/ex02/main.cpp
+++ Module 07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mouassit <mouassit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 13:25:26 by mouassit          #+#    #+#             */
-/*   Updated: 2022/04/30 14:41:54 by mouassit         ###   ########.fr       */
+/*   Updated: 2022/04/30 14:54:19 by mouassit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ int main()
 {
     Array<std::string> arrayOne;
     Array <std::string> arrayTwo(10);
+    
     std::cout << "len arrayOne :  " << arrayOne.size() << std::endl;
+    std::cout << "len arrayTwo = "<< arrayTwo.size() << std::endl;
+    
     arrayTwo[8] = "test";
+    
     Array <std::string> arrayThree(arrayTwo);
+    
     std::cout << "len arrayThree = "<< arrayThree.size() << std::endl;
     std::cout << "value arrayThree[8] = " << arrayThree[8] << std::endl;
     return 0;
